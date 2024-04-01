2425bf46ab736f7b6bcad4523c55a38eba9e88bb
CarmSC <carmencabrera2035@gmail.com>
2024-04-01 16:15:15 +0200

    new class Params

---
+++ src/main/java/com/carmen/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
@@ -0,0 +1,34 @@
+package com.carmen.curso.springboot.webapp.springbootwebapp.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carmen.curso.springboot.webapp.springbootwebapp.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+    
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+    
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
+    }
+}
---
+++ src/main/java/com/carmen/curso/springboot/webapp/springbootwebapp/models/dto/ParamDto.java
@@ -0,0 +1,17 @@
+package com.carmen.curso.springboot.webapp.springbootwebapp.models.dto;
+
+public class ParamDto {
+    
+    private String message;
+
+    
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
