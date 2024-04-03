31e2c64f521b376ec7b82f77926654a053402a86
CarmSC <carmencabrera2035@gmail.com>
2024-04-03 19:15:45 +0200

    commit HomeController

--- src/main/java/com/carmen/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
+++ src/main/java/com/carmen/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +25,6 @@ public class PathVariableController {
     @Value("${config.username}")
     private String username;
 
-    //@Value("${config.message}")
-    // private String message;
-
     @Value("${config.listOfValues}")
     private List<String> listOfValues;
 
@@ -37,10 +36,19 @@ public class PathVariableController {
 
     @Value("${config.code}")
     private Integer code;
-    
-    @Value("#{${config.valueMap}}")
+
+    @Value("#{${config.valuesMap}}")
     private Map<String, Object> valueMap;
-    
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Value("#{${config.valuesMap}.price}")
+    private Long price;
+
+    @Autowired
+    private Environment environment;
+        
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
 
@@ -62,21 +70,27 @@ public class PathVariableController {
     public User create(@RequestBody User user) {
         
         user.setName(user.getName().toUpperCase());
-        //user.setLastname(user.getLastname().toUpperCase());
+        user.setLastname(user.getLastname().toUpperCase());
         return user;
     }
 
     @GetMapping("/values")
     public Map<String, Object> values(@Value("${config.message}") String message ) {
 
+        Long code2 = environment.getProperty("config.code", Long.class);
+
         Map<String, Object> json = new HashMap<>();
         json.put("username", username);
         json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("code2", code2);
         json.put("code", code);
         json.put("listOfValues", listOfValues);
         json.put("valueList", valueList);
         json.put("valueString", valueString);
         json.put("valueMap", valueMap);
+        json.put("product", product);
+        json.put("price", price);
         return json;
 
     }
